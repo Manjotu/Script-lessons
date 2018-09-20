bc8bcbdad37e4dc6fe466b8d1dcf136b2503a531
Manjotu <m.uppal3@share.epsb.ca>
2018-09-20 12:56:38 -0600

    new

--- Index.js
+++ Index.js
@@ -1,7 +1,13 @@
 console.log('Hello World')
 
 // Create 10 variables about yourself
-let name = "Manjot"
-
+let firstName = "Manjot"
+let lastName = "Uppal"
+let age = 16;
+const ageAfraid = 22; //security feature
+let hair = "Black"
 // Print variables to console
-console.log(name);
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hair);
