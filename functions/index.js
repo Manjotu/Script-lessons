05923d62620f202f66f1e0d6a24751cf4159c645
Manjotu <m.uppal3@share.epsb.ca>
2018-09-24 13:57:53 -0600

    new

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+//Hello World Function
+function greet (firstName, lasName ) {
+  console.log('Hello' + firstName + 'World' + lasName);
+}
+
+function square(number) {
+  return number*number;
+}
+//firstName, lastName, age, male, female
+let ManjotUppal = ["Manjot", "Uppal" 16, true, false]
+
+//calling functions, with argument
+greet (ManjotUppal[0], ManjotUppal[1])
+
+
+
+
+let ManjotUppal = [];
+//  firstName, lasName, age, male, female
+ManjotUppal = ["Manjot", "Uppal", 16, true, false];
+
+console.log(ManjotUppal);
+console.log(ManjotUppal.firstName);
+
+
+console.log("last Name is: " + ManjotUppal[1]);
+
+ManjotUppal[1]="Is Awesome";
+console.log(ManjotUppal);
