062e80557711a45801347dc229732282e7a08077
Manjotu <m.uppal3@share.epsb.ca>
2018-09-24 13:30:06 -0600

    new

---
+++ Swapping variables function/index.js
@@ -0,0 +1,25 @@
+// Create 10 variables about yourself
+let ManjotUppal = [];
+//  firstName, lasName, age, male, female
+ManjotUppal = ["Manjot", "Uppal", 16, true, false];
+
+console.log(ManjotUppal);
+console.log(ManjotUppal.firstName);
+
+
+console.log("last Name is: " + ManjotUppal[1]);
+
+ManjotUppal[1]="Is Awesome";
+console.log(ManjotUppal);
+
+
+
+
+
+// Add a third variables
+//let memory = firstName;
+//firstName = lastName;
+//lasName = memory;
+//console.log("I am male?");
+//.log("My firstName is Uppal" + firstName);
+//console.log("My lastName is Manjot" + lastName);
