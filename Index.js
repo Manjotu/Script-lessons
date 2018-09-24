0638bcb507cedf59c0fbf99fd05a4b4f1817d26a
Manjotu <m.uppal3@share.epsb.ca>
2018-09-24 12:39:31 -0600

    new

--- Index.js
+++ Index.js
@@ -10,4 +10,10 @@ console.log(firstName);
 console.log(lastName);
 console.log(age);
 console.log(hair);
+// Add a third variables
+let memory = firstName;
+firstName = lastName;
+lasName = memory;
 console.log("I am male?");
+console.log("My firstName is Uppal" + firstName);
+console.log("My lastName is Manjot" + lastName);
---
+++ Swapping Variables Object Start/index.js
@@ -0,0 +1,27 @@
+// Create 10 variables about yourself
+let ManjotUppal = {
+  firstName: "Manjot",
+  lasName: "Uppal",
+  age: 16,
+  male: true,
+}
+
+//printing out entire objects
+console.log(ManjotUppal.firstName);
+// Print variables to console
+//console.log(firstName);
+//console.log(lastName);
+//console.log(age);
+//console.log(hair);
+
+
+
+
+
+// Add a third variables
+//let memory = firstName;
+//firstName = lastName;
+//lasName = memory;
+//console.log("I am male?");
+//.log("My firstName is Uppal" + firstName);
+//console.log("My lastName is Manjot" + lastName);
