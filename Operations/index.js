ba7010f969a35a7609273f691822e79d921f4f2a
Manjotu <m.uppal3@share.epsb.ca>
2018-09-26 13:44:12 -0600

    new

---
+++ Operations/index.js
@@ -0,0 +1,19 @@
+console.log('Hello World')
+
+// Create 10 variables about yourself
+let firstName = "Manjot"
+let lasName = "Uppal"
+let age = 16;
+let male = true;
+// Print variables to console
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hair);
+// Add a third variables
+let memory = firstName;
+firstName = lastName;
+lasName = memory;
+console.log("I am male?");
+console.log("My firstName is Uppal" + firstName);
+console.log("My lastName is Manjot" + lastName);
