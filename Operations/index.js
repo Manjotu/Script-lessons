c7ddf36fea2e8c610629319bdba2e51ade97f9c5
Manjotu <m.uppal3@share.epsb.ca>
2018-09-26 14:13:49 -0600

    new

--- Operations/index.js
+++ Operations/index.js
@@ -1,19 +1,16 @@
-console.log('Hello World')
+function simpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtract = numberA - numberB;
+  let mutliply = numberA * numberB;
+  let divide = numberA / numberB;
+  console.log(adding);
+  console.log(subtract);
+  console.log(mutliply);
+  console.log(divide);
+}
 
-// Create 10 variables about yourself
-let firstName = "Manjot"
-let lasName = "Uppal"
-let age = 16;
-let male = true;
-// Print variables to console
-console.log(firstName);
-console.log(lastName);
-console.log(age);
-console.log(hair);
-// Add a third variables
-let memory = firstName;
-firstName = lastName;
-lasName = memory;
-console.log("I am male?");
-console.log("My firstName is Uppal" + firstName);
-console.log("My lastName is Manjot" + lastName);
+let number = [4, 7];
+simpleOperators (number[0], number[1]);
+
+
+let skipCount
